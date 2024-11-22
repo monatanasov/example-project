bf5cdeb7cce8a6539fd20e004ab1fa7b071e9e87
monatanasov <simeon.g.atanasov@gmail.com>
2024-11-22 18:13:04 +0200

    add project/task UNIT tests with TODO info only

---
+++ tests/Unit/ProjectTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ProjectTest extends TestCase
+{
+    // TODO: test relationship with Tasks
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/TaskTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class TaskTest extends TestCase
+{
+    // TODO: test relationship with Project
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
