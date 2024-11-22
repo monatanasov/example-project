56a0073ade34fcb676980f28a85f60c87cb0bccb
monatanasov <simeon.g.atanasov@gmail.com>
2024-11-22 19:02:42 +0200

    Show all projects in the reports page upon loading. Refactor the design.

--- resources/js/components/Reports.vue
+++ resources/js/components/Reports.vue
@@ -1,21 +1,63 @@
 <template>
     <div class="w-1/4">
-        <h3>Report Page</h3>
+        <div>
+            <h3 class="flex justify-center">Reports Table</h3>
+            <table v-if="projects.length">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="project in projects" :key="project.id">
+                        <td>{{ project.id }}</td>
+                        <td>{{ project.name }}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <p v-else>No projects available</p>
+        </div>
+
+        <div class="flex justify-between">
+            <router-link class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" to="/projects">
+                Go to Projects Page
+            </router-link>
+            <router-link class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" to="/tasks">
+                Go to Tasks Page
+            </router-link>
+        </div>
     </div>
 
-    <router-link class="text-blue-700 flex" to="/projects">
-        Go to Projects Page
-    </router-link>
-    <router-link class="text-blue-700 flex" to="/tasks">
-        Go to Tasks Page
-    </router-link>
+
+
+    {{ this.projects }}
+
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: 'reports',
     data() {
-        return {};
+        return {
+            projects: [],
+        };
     },
+    mounted() {
+        this.fetchProjects();
+    },
+    methods: {
+        fetchProjects() {
+            axios.get('/projects')
+                .then(response => {
+                    this.projects = response.data.data;
+                })
+                .catch(error => {
+                    console.error("There was an error fetching the projects data:", error);
+                });
+        }
+    }
 }
 </script>
--- resources/js/components/Tasks.vue
+++ resources/js/components/Tasks.vue
@@ -1,11 +1,15 @@
 <template>
-    <h3>Hello from Tasks</h3>
-    <router-link class="text-blue-700 flex" to="/projects">
-        Go to Projects Page
-    </router-link>
-    <router-link class="text-blue-700 flex" to="/reports">
-        Go to Reports Page
-    </router-link>
+    <div class="w-1/4">
+        <h3>Hello from Tasks</h3>
+        <div class="flex justify-between">
+            <router-link class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" to="/projects">
+                Go to Projects Page
+            </router-link>
+            <router-link class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" to="/reports">
+                Go to Reports Page
+            </router-link>
+        </div>
+    </div>
 </template>
 
 <script>
